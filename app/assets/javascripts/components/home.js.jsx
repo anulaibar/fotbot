c1d3afe29b215e87ee90536277e67ae6a9cb39fc
Olle Hellgren <olle.hellgren@gmail.com>
2015-05-06 00:48:50 +0200

    Styling buttons on home screen

--- app/assets/javascripts/components/home.js.jsx
+++ app/assets/javascripts/components/home.js.jsx
@@ -5,8 +5,8 @@ var Home = React.createClass({
   render: function() {
     return (
       <div>
-        <Link to="teams">Teams</Link>
-        <Link to="players">Players</Link>
+        <Link to="teams" className="g-1-2 teams-button">Teams</Link>
+        <Link to="players" className="g-1-2 players-button">Players</Link>
       </div>
     );
   }
