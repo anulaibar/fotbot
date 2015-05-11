b4abd08642e4f33f2fcb3ea8ce8344dbbbd72edc
Olle Hellgren <olle.hellgren@gmail.com>
2015-05-11 23:29:36 +0200

    Added delete player functionality

--- app/assets/javascripts/components/show_player.js.jsx
+++ app/assets/javascripts/components/show_player.js.jsx
@@ -2,6 +2,8 @@ var Router = ReactRouter;
 var Link = Router.Link;
 
 var ShowPlayer = React.createClass({
+  mixins: [Router.Navigation],
+
   getInitialState: function() {
     return {
       player: {name: null, avatar: null}
@@ -9,6 +11,21 @@ var ShowPlayer = React.createClass({
   },
   contextTypes: {
     router: React.PropTypes.func
+  },
+  handleClick: function() {
+    // Get player id from the url
+    var playerId = this.context.router.getCurrentParams().id;
+    var url = 'players/' + playerId;
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      success: function(data) {
+        this.transitionTo('players');
+      }.bind(this),
+      error: function(xhr) {
+        console.error(xhr.responseJSON.errors.toString());
+      }.bind(this)
+    });
   },
   componentDidMount: function() {
     // Get player id from the url
@@ -28,9 +45,15 @@ var ShowPlayer = React.createClass({
         <div className="avatar">
           <img src={this.state.player.avatar}/>
         </div>
-        <div className="g-1 purple">
+        <div className="g-1-4 purple">
+          &nbsp;
+        </div>
+        <div className="g-2-4 purple">
           {this.state.player.name}
         </div>
+        <button className="g-1-4 red" onClick={this.handleClick}>
+          Delete
+        </button>
       </div>
     );
   }
