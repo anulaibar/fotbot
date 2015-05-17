8db5f5f60c2d380fa33939c6cbc462264950d034
Olle Hellgren <olle.hellgren@gmail.com>
2015-05-18 00:10:34 +0200

    Broke out create player component

---
+++ app/assets/javascripts/components/create_player.js.jsx
@@ -0,0 +1,39 @@
+var CreatePlayer = React.createClass({
+  getInitialState: function() {
+    return { creating: false };
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.setState({creating: true});
+    var input = React.findDOMNode(this.refs.name);
+    var name = input.value.trim();
+    var data = {player: {name: name}};
+    $.ajax({
+      url: 'players',
+      dataType: 'json',
+      method: 'POST',
+      data: data,
+      context: this
+    })
+    .done(function(response) {
+      // Pass the newly create player on up the component chain
+      this.props.onCreate(response);
+    })
+    .fail(function(xhr) {
+      console.error(xhr.responseJSON.errors.toString());
+    })
+    .always(function() {
+      this.setState({creating: false});
+      input.value = '';
+    })
+  },
+  render: function() {
+    var buttonText = this.state.creating ? 'Creating...' : 'Create';
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input className="g-2-3" ref="name" placeholder="Enter name"/>
+        <button className="g-1-3 green">{buttonText}</button>
+      </form>
+    );
+  }
+});
--- app/assets/javascripts/components/new_player.js.jsx
+++ app/assets/javascripts/components/new_player.js.jsx
@@ -6,38 +6,20 @@ var NewPlayer = React.createClass({
     e.preventDefault();
     this.setState({showForm: !this.state.showForm})
   },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var name = React.findDOMNode(this.refs.name).value.trim();
-    var data = {player: {name: name}};
-    $.ajax({
-      url: 'players',
-      dataType: 'json',
-      method: 'POST',
-      data: data,
-      context: this
-    })
-    .done(function(response) {
-      this.props.onCreate(response);
-    })
-    .fail(function(xhr) {
-      console.error(xhr.responseJSON.errors.toString());
-    })
-    .always(function() {
-      this.setState({showForm: false});
-    })
+  handleCreate: function(player) {
+    this.setState({showForm: false});
+    // Pass the newly create player on up to the players component
+    // so that it can be added to the players list
+    this.props.onCreate(player);
   },
   render: function() {
-    var classes = this.state.showForm ? 'form' : 'form hide';
+    var classes = this.state.showForm ? 'create-player' : 'create-player hide';
     return (
       <div>
         <div className="g-2-3" />
         <a href="" onClick={this.handleClick} className="g-1-3 purple">New</a>
         <div className={classes}>
-          <form onSubmit={this.handleSubmit}>
-            <input className="g-2-3" ref="name" placeholder="Enter name"/>
-            <button className="g-1-3 green">Create</button>
-          </form>
+          <CreatePlayer onCreate={this.handleCreate}/>
         </div>
       </div>
     );
