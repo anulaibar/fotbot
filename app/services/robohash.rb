3a24a8d06c6efd732af0b52520b589ec97c0d5da
Olle Hellgren <olle.hellgren@gmail.com>
2015-05-17 12:26:14 +0200

    Logging if robohash api key is unset Calling api only if key is set

--- app/services/robohash.rb
+++ app/services/robohash.rb
@@ -4,17 +4,24 @@ class Robohash
   attr_accessor :api_key
 
   def initialize
-    @api_key = ENV['ROBOHASH_API_KEY']
+    if ENV['ROBOHASH_API_KEY']
+      @api_key = ENV['ROBOHASH_API_KEY']
+    else
+      Rails.logger.error('Environment variable ROBOHASH_API_KEY is unset')
+    end
   end
 
   def fetch(string)
-    escaped = URI.escape(string)
-    url = "https://robohash.p.mashape.com/index.php?text=#{escaped}"
-    begin
-      response = RestClient.get url, {:accept => :json, x_mashape_key: api_key}
-      image_url = JSON.parse(response.body)['imageUrl']
-    rescue RestClient::BadRequest => e
-      Rails.logger.error(e)
+    if api_key
+      escaped = URI.escape(string)
+      url = "https://robohash.p.mashape.com/index.php?text=#{escaped}"
+      begin
+        response = RestClient.get url, {:accept => :json, x_mashape_key: api_key}
+        image_url = JSON.parse(response.body)['imageUrl']
+      rescue RestClient::BadRequest => e
+        byebug
+        Rails.logger.error(e)
+      end
     end
     image_url || 'robot.png'
   end
